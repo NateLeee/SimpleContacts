5a8095ee7d7b4748868e86dcc8a27b5bb42a7b50
Nate Lee <nateleee.dev@gmail.com>
2020-08-10 11:39:39 +0800

    Initial Commit

---
+++ SimpleContacts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleContacts
+//
+//  Created by Nate Lee on 8/10/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
