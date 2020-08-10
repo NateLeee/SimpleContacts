b360edec5d37aa4d070485344c184d558f02ee80
Nate Lee <nateleee.dev@gmail.com>
2020-08-10 21:24:59 +0800

    Added location usage description & added a new class to fetch location info.

---
+++ SimpleContacts/Helpers/LocationFetcher.swift
@@ -0,0 +1,14 @@
+//
+//  LocationFetcher.swift
+//  SimpleContacts
+//
+//  Created by Nate Lee on 8/10/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import CoreLocation
+
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    
+}
